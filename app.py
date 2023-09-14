2eaaa49eb41994a89f5c5d8cdb0f06df4c93d46f
jojo <jojoryan.ramirez@gmail.com>
2023-09-14 23:00:28 +0800

    added sample data for looping

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	mountains = ['everest','k2','k3']
+	return render_template('index.html', mountain=mountains)
+
+@app.route('/mountains/<mt>')
+def mountain(mt):
+	return "This is " + str(mt)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
