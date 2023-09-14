d71c1b6a0cfcd3fbf115a4d79e99a25bc14ce467
jojo <jojoryan.ramirez@gmail.com>
2023-09-14 14:28:42 +0000

    app changes

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-test file
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return "Hello"
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000, debug=True)
