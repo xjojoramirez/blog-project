596cb1d80dfb9e0c98159dc843d641257c86a6d4
jojo <jojoryan.ramirez@gmail.com>
2023-09-16 01:05:47 +0800

    add pages, views and navbar

--- app.py
+++ app.py
@@ -11,5 +11,28 @@ def index():
 def mountain(mt):
 	return "This is " + str(mt)
 
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+
+#Custom error pages
+
+#invalid URL
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template("404.html"), 404
+
+#internal server error
+@app.errorhandler(500)
+def page_not_found(error):
+	return render_template('500.html'), 500
+
+
+
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
